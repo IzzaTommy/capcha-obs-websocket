82c5a6a8be975c4d9f3a05ec38adba589067b350
IzzaTommy <42631988+IzzaTommy@users.noreply.github.com>
2025-03-12 16:30:21 -0700

    changed default server enabled to true

--- src/Config.h
+++ src/Config.h
@@ -34,7 +34,7 @@ struct Config {
 	std::atomic<bool> PasswordOverridden = false;
 
 	std::atomic<bool> FirstLoad = true;
-	std::atomic<bool> ServerEnabled = false;
+	std::atomic<bool> ServerEnabled = true;
 	std::atomic<uint16_t> ServerPort = 4455;
 	std::atomic<bool> Ipv4Only = false;
 	std::atomic<bool> DebugEnabled = false;
